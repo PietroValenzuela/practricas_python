9a2baebffead58ef2251af7b4a28f337be0f43f3
PietroValenzuela <pietro.mv@icloud.com>
2024-08-02 10:17:58 -0400

    in.py

---
+++ f_string.py
@@ -0,0 +1,11 @@
+#ejemplos de uso f-string
+nombre="Pietro"
+edad= 28
+print(f"Hola,{nombre}, Tienes {edad} años")
+#ejemplos de suma f-string
+precio1= 78293
+precio2= 312
+print(f"El total es: ${precio1 + precio2}")
+#if dentro de un print
+temperatura= 30
+print(f"La temperatura esta {"alta" if temperatura>25 else "baja"}")
